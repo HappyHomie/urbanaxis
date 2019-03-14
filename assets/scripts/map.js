1e84f3b23ce04c3b43278acd30be8331e8ed09a7
Arne Emil Buchter <5651612@RTS-365.dk>
2019-03-14 13:50:36 +0100

    cleanup

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
-    //  console.log("Min Dom blev indlæst");
+
     var backlink = document.querySelector('.setup__prior');
-    // console.log(backlink);
+
     backlink.addEventListener('click', function(){
         history.back();
     });
