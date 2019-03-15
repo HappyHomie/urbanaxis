89af09595622683d23081b19a31d65fbabb7c217
Mathias Ferniss <5211966@RTS-365.dk>
2019-03-15 12:03:06 +0100

    all

---
+++ assets/scripts/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var register = document.querySelector('.start-button');
+    register.addEventListener('click', function(){
+        alert("hej")
+    });
+});
