2b8f98097d0d04c0b6db84e6d9b9d1960cd59d02
Arne Emil Buchter <5651612@RTS-365.dk>
2019-03-15 11:30:33 +0100

    bugfix

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -2,13 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var backlink = document.querySelector('.setup__prior');
 
-   
-  
+    backlink.addEventListener('click', function(){
+      history.back();
+    });
 
+});
 
-  backlink.addEventListener('click', test);
-
-  function test () {
-      history.back(-1);
-  }
-})
