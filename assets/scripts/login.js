2fe0ff9e1a5426dcbcca83224ec656c427bad552
Mathias Ferniss <5211966@RTS-365.dk>
2019-03-15 13:21:42 +0100

    done

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     var register = document.querySelector('.start-button');
     register.addEventListener('click', function(){
-        alert("hej")
+
+        var pw1 = document.querySelector('#pw2');
+        var pw2 = document.querySelector('#pw2');
+        
+        console.log(pw1+" "+pw2);
+        
     });
+    
 });
