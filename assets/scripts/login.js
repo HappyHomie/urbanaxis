b1049e09741cd1faeca13b41b2d221a618c8e659
Mathias Ferniss <5211966@RTS-365.dk>
2019-03-20 09:03:50 +0100

    baggrundsbillede på login siderne

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     var register = document.querySelector('.start-button');
     register.addEventListener('click', function(){
-
         var pw1 = document.querySelector('#pw2');
         var pw2 = document.querySelector('#pw2');
-        
-        console.log(pw1+" "+pw2);
-        
     });
     
 });
