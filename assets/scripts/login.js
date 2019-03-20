5b5679ca0ff17557e42c5f883ead2cd3051ea652
Mathias Ferniss <5211966@RTS-365.dk>
2019-03-20 13:15:19 +0100

    ITS THE FINAL COUNTDOWN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -3,17 +3,19 @@ document.addEventListener('DOMContentLoaded', function(){
     register.addEventListener('click', function(){
         var pw1 = document.querySelector('#pw1');
         var pw2 = document.querySelector('#pw2');
+        var username = document.querySelector('#username');
+        var mail = document.querySelector('#mail');
 
         if(pw1.value == "" || pw2.value == ""){
             alert("Udfyldt venligst begge password felter.");
+        }else if(username.value == "" || mail.value == "") {
+                alert("udfyld venligst alle felter");
         }else if(pw1.value !== pw2.value){
             alert("De to password er ikke ens!");
+        }else if(mail.value.indexOf("@")==-1){
+            alert("Lav rigtig email :)");
         }else{
             window.location.href="index.html";
         }
-    }); 
-    register.addEventListener('click', function(){
-        var username = document.querySelector('#username');
-        var mail = document.querySelector('#mail');
     });
 });
