a452326e5b075293143d79af2ee371ca22ba2cb1
Mathias Ferniss <5211966@RTS-365.dk>
2019-03-20 11:13:48 +0100

    js igang

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -3,10 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
     register.addEventListener('click', function(){
         var pw1 = document.querySelector('#pw1');
         var pw2 = document.querySelector('#pw2');
-        if (pw1.value == pw2.value) {
+
+        if(pw1.value == "" || pw2.value == ""){
+            alert("Udfyldt venligst begge password felter.");
+        }else if(pw1.value !== pw2.value){
+            alert("De to password er ikke ens!");
+        }else{
             window.location.href="index.html";
-        } else {
-            alert("FJOLS");
         }
+    }); 
+    register.addEventListener('click', function(){
+        var username = document.querySelector('#username');
+        var mail = document.querySelector('#mail');
     });
 });
